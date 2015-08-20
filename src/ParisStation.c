5930f949ca934a5bbf4db3465e828805ef0f4666
Thibaut LE LEVIER <thibaut@lelevier.fr>
2015-08-20 18:40:30 +0200

    menu layer and js web call

--- src/ParisStation.c
+++ src/ParisStation.c
@@ -1,43 +1,64 @@
 #include <pebble.h>
 
 static Window *window;
-static TextLayer *text_layer;
 
-static void select_click_handler(ClickRecognizerRef recognizer, void *context) {
-  text_layer_set_text(text_layer, "Select");
+static MenuLayer *menu_layer;
+
+// menu layer
+static uint16_t menu_get_num_sections_callback(MenuLayer *menu_layer, void *data) {
+  return 1;
 }
 
-static void up_click_handler(ClickRecognizerRef recognizer, void *context) {
-  text_layer_set_text(text_layer, "Up");
+static uint16_t menu_get_num_rows_callback(MenuLayer *menu_layer, uint16_t section_index, void *data) {
+  return 10;
 }
 
-static void down_click_handler(ClickRecognizerRef recognizer, void *context) {
-  text_layer_set_text(text_layer, "Down");
+static int16_t menu_get_header_height_callback(MenuLayer *menu_layer, uint16_t section_index, void *data) {
+  return 0;
 }
 
-static void click_config_provider(void *context) {
-  window_single_click_subscribe(BUTTON_ID_SELECT, select_click_handler);
-  window_single_click_subscribe(BUTTON_ID_UP, up_click_handler);
-  window_single_click_subscribe(BUTTON_ID_DOWN, down_click_handler);
+static void menu_draw_header_callback(GContext* ctx, const Layer *cell_layer, uint16_t section_index, void *data) {
+  menu_cell_basic_header_draw(ctx, cell_layer, "Test");
 }
 
+static void menu_draw_row_callback(GContext* ctx, const Layer *cell_layer, MenuIndex *cell_index, void *data) {
+  // menu_cell_basic_draw(ctx, cell_layer, "Test", NULL, NULL);
+  menu_cell_title_draw(ctx, cell_layer, "Test");
+}
+
+static void menu_select_callback(MenuLayer *menu_layer, MenuIndex *cell_index, void *data) {
+  APP_LOG(APP_LOG_LEVEL_DEBUG, "Select %i", cell_index->row);
+}
+
+// window
 static void window_load(Window *window) {
   Layer *window_layer = window_get_root_layer(window);
   GRect bounds = layer_get_bounds(window_layer);
 
-  text_layer = text_layer_create((GRect) { .origin = { 0, 72 }, .size = { bounds.size.w, 20 } });
-  text_layer_set_text(text_layer, "Press a button");
-  text_layer_set_text_alignment(text_layer, GTextAlignmentCenter);
-  layer_add_child(window_layer, text_layer_get_layer(text_layer));
+  menu_layer = menu_layer_create(bounds);
+
+  menu_layer_set_callbacks(menu_layer, NULL, (MenuLayerCallbacks){
+    .get_num_sections = menu_get_num_sections_callback,
+    .get_num_rows = menu_get_num_rows_callback,
+    .get_header_height = menu_get_header_height_callback,
+    .draw_header = menu_draw_header_callback,
+    .draw_row = menu_draw_row_callback,
+    .select_click = menu_select_callback,
+  });
+
+  menu_layer_set_click_config_onto_window(menu_layer, window);
+
+  layer_add_child(window_layer, menu_layer_get_layer(menu_layer));
 }
 
 static void window_unload(Window *window) {
-  text_layer_destroy(text_layer);
+
+  menu_layer_destroy(menu_layer);
 }
 
+// init/deinit
 static void init(void) {
   window = window_create();
-  window_set_click_config_provider(window, click_config_provider);
   window_set_window_handlers(window, (WindowHandlers) {
     .load = window_load,
     .unload = window_unload,
@@ -50,6 +71,7 @@ static void deinit(void) {
   window_destroy(window);
 }
 
+// main
 int main(void) {
   init();
 
--- src/js/pebble-js-app.js
+++ src/js/pebble-js-app.js
@@ -1,5 +1,47 @@
+var baseURL = 'https://secure-atoll-4691.herokuapp.com';
+
+function fetchStations(latitude, longitude) {
+
+  var url = baseURL + '/metro/stations';
+
+  if (latitude && longitude) {
+    url = url + '?ll=' + latitude + ',' + longitude;
+  }
+
+  var req = new XMLHttpRequest();
+  req.open('GET', url, true);
+  req.onload = function () {
+    if (req.readyState === 4) {
+      if (req.status === 200) {
+        console.log(req.responseText);
+        var response = JSON.parse(req.responseText);
+        Pebble.sendAppMessage(response);
+      } 
+      else {
+        console.log('Error');
+      }
+    }
+  };
+  req.send(null);
+}
+
+function locationSuccess(pos) {
+  fetchStations(pos.coords.latitude, pos.coords.longitude);
+}
+
+function locationError(err) {
+  fetchStations();
+}
+
 Pebble.addEventListener("ready",
     function(e) {
-        console.log("Hello world! - Sent from your javascript application.");
+      console.log('ready from js');
+
+      return;
+      var locationOptions = {
+        'timeout': 15000,
+        'maximumAge': 60000
+      };
+      window.navigator.geolocation.getCurrentPosition(locationSuccess, locationError, locationOptions);
     }
 );
