a17e6dbfcf9ccea5c4853b926060a571969f57f4
Thibaut LE LEVIER <thibaut@lelevier.fr>
2015-09-20 00:38:25 +0200

    rewrite in js with pebblejs

--- src/js/app.js
+++ src/js/app.js
@@ -1,8 +1,179 @@
 var UI = require('ui');
-var Vector2 = require('vector2');
 
-var loaderCard = new UI.Card({
-  body: 'Loading...'
-});
+// API
+var baseURL = 'https://secure-atoll-4691.herokuapp.com';
 
-loaderCard.show();
+function fetchStations(latitude, longitude, callback) {
+
+  var url = baseURL + '/metro/stations';
+
+  if (latitude && longitude) {
+    url = url + '?ll=' + latitude + ',' + longitude + '&limit=5';
+  }
+  
+  var req = new XMLHttpRequest();
+  req.open('GET', url, true);
+  req.onload = function () {
+    if (req.readyState === 4) {
+      if (req.status === 200) {
+        var response = JSON.parse(req.responseText);
+        var stations = response.stations;
+        callback(stations);
+      } 
+      else {
+        showError(null);
+      }
+    }
+  };
+  req.send(null);
+}
+
+function fetchLinesForStation(station_key, callback) {
+  var url = baseURL + '/metro/stations/' + station_key.replace(/ /g,'%20') + '/lines';
+
+  var req = new XMLHttpRequest();
+  req.open('GET', url, true);
+  req.onload = function () {
+    if (req.readyState === 4) {
+      if (req.status === 200) {
+        var response = JSON.parse(req.responseText);
+        var lines = response.lines;
+        callback(lines);
+      } 
+      else {
+        showError(null);
+      }
+    }
+  };
+  req.send(null);
+}
+
+function fetchSchedules(station_key, line, direction, callback) {
+
+  var url = baseURL + '/metro/stations/' + station_key.replace(/ /g,'%20') + '/lines/' + line + '/directions/' + direction + '/schedules';
+
+  var req = new XMLHttpRequest();
+  req.open('GET', url, true);
+  req.onload = function () {
+    if (req.readyState === 4) {
+      if (req.status === 200) {
+        var response = JSON.parse(req.responseText);
+        var schedules = response.schedules;
+        callback(schedules);
+      } 
+      else {
+        showError(null);
+      }
+    }
+  };
+  req.send(null);
+}
+
+// UI
+function showError(error_message){
+  var card = new UI.Card({
+    title: 'Error',
+    body: (error_message?error_message:"Something went wrong!"),
+    scrollable: false
+  });
+  
+  card.show();
+}
+
+function setupStationsList(stations){
+  var stations_list = new UI.Menu({
+    sections: [{
+      title: 'Nearby',
+      items: stations
+    }]
+  });
+  
+  stations_list.show();
+  
+  stations_list.on('select', function(e) {    
+    fetchLinesForStation(e.item.key, function(lines, error){
+      var ui_lines = Array();
+      
+      lines.forEach(function(line){
+        var l = {'title' : line.line, 'station_key': e.item.key};
+        var items = Array();
+        line.destinations.forEach(function(destination){
+          items.push({
+            'title': destination.name,
+            'direction': destination.direction
+          });
+        });
+        l.items = items;
+        
+        ui_lines.push(l);
+        setupLinesList(ui_lines);
+      });
+    });
+  });
+}
+
+function setupLinesList(lines){
+  var lines_list = new UI.Menu({
+    sections: lines
+  });
+  
+  lines_list.show();
+  
+  lines_list.on('select', function(e) {    
+    fetchSchedules(e.section.station_key, e.section.title, e.item.direction, function(schedules, error){
+      var ui_schedules = Array();
+      
+      schedules.forEach(function(schedule){
+        ui_schedules.push({
+          'title' : schedule.destination,
+          'items' : [{
+            'title' : schedule.arriving
+          }]
+        });
+      });
+      
+      setupSchedulesList(ui_schedules);
+    });
+  });
+}
+
+function setupSchedulesList(schedules){
+  var schedule_list = new UI.Menu({
+    sections: schedules
+  });
+  
+  schedule_list.show();
+}
+
+// geolocation
+function locationSuccess(pos) {
+  fetchStations(pos.coords.latitude, pos.coords.longitude, function(stations){
+    formatStations(stations);
+  });
+}
+
+function locationError(err) {
+  fetchStations(null, null, function(stations){
+    formatStations(stations);
+  });
+}
+
+function formatStations(stations){
+  var ui_stations = Array();
+    
+    stations.forEach(function(station){
+      ui_stations.push({
+        'title': station.name,
+        'key': station.key
+      });
+    });
+    
+    setupStationsList(ui_stations);
+}
+
+var locationOptions = {
+  'timeout': 15000,
+  'maximumAge': 60000
+};
+
+window.navigator.geolocation.getCurrentPosition(locationSuccess, locationError, locationOptions);
